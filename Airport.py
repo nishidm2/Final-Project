bf6faa363a5649af6e0e8ff8fedb734e592349db
devanshgandhi <devanshgandhi3@gmail.com>
2018-12-06 15:47:02 -0600

    Case 3

--- Airport.py
+++ Airport.py
@@ -62,7 +62,6 @@ class Agent:
 
 
     def get_Agent_Rate(self):
-
         return 300
 
     def Serving_Time(agent_num,rate):
@@ -98,7 +97,7 @@ amount: no of pass served by the agent
     def passengers_served(agent_rate, agent_num):
         pass_num = 5400 / agent_rate
         total_pass_num = pass_num * agent_num
-        return pass_num
+        return int(total_pass_num)
 
     def getAgent(self):
         """
@@ -128,9 +127,9 @@ class Flight:
         return
 
     def gen_flight(self):
-        process = [100, 150, 200]
-        weights = [0, 1.5, 3]
-        return choice(process, weights)
+        process = [[100, 150, 200],[200,200,200],[200,100,200],[100,150,150],[150,200,100]]
+
+        return choice(process)
 
 
 def simulate(pass_num,agent_num):
@@ -145,13 +144,11 @@ def simulate(pass_num,agent_num):
     no_of_pass =int( pass_num / agent_num)
     queue_len = Passenger.queue_length(Passenger, no_of_pass)
     #print(serving_time)
+
+
     for i in range(pass_num):
         # When the customer has arrived at the restaurant and is pushed into the queue
         Passenger.waiting_time_queue = queue_len * serving_time
-        # print(queue_len)
-
-        # print(queue_len*serving_time)
-        #print(Passenger.waiting_time_queue)
         queue.enqueue(Passenger.getPass(Passenger))
         # When the customer at the counter has been served
         if (not airport_agent.isBusy() and (not queue.isEmpty())):
@@ -173,11 +170,15 @@ def flight_simulator(flight_passenger1,flight_passenger2,flight_passenger3,agent
     checkin_agent = Agent_Serves_Passenger(agent_num, agent_rate)
     serving_time = Agent.Serving_Time(agent_num, agent_rate)
 
-    served=Agent_Serves_Passenger.passengers_served()
+    served=Agent_Serves_Passenger.passengers_served(agent_rate,agent_num)
+
     if flight_passenger1 < served:
         pass_num=flight_passenger1
+    #     pass_num agents will serve in 1.5 hrs
     else:
         pass_num=served
+        pass_num_remaining = flight_passenger1 - served
+        flight_passenger2 += pass_num_remaining
 
     no_of_pass = int(pass_num / agent_num)
     queue_len = Passenger.queue_length(Passenger, no_of_pass)
@@ -185,14 +186,9 @@ def flight_simulator(flight_passenger1,flight_passenger2,flight_passenger3,agent
 
 
 
-
     for i in range(pass_num):
         # When the customer has arrived at the restaurant and is pushed into the queue
         Passenger.wait_time_queue = queue_len * serving_time
-        # print(queue_len)
-
-        # print(queue_len*serving_time)
-        #print(Passenger.waiting_time_queue)
         queue1.enqueue(Passenger.getPass(Passenger))
         # When the customer at the counter has been served
         if (not checkin_agent.isBusy() and (not queue1.isEmpty())):
@@ -204,7 +200,54 @@ def flight_simulator(flight_passenger1,flight_passenger2,flight_passenger3,agent
         #print(queue)
         checkin_agent.setIdle()  # current customer has now left and the cashier is available to serve the next customer
 
-    #print(waiting_Times)
+    if flight_passenger2 < served:
+        pass_num = flight_passenger2
+    else:
+        pass_num = served
+        pass_num_remaining = flight_passenger2 - served
+        flight_passenger3 += pass_num_remaining
+
+    no_of_pass = int(pass_num / agent_num)
+    queue_len = Passenger.queue_length(Passenger, no_of_pass)
+
+
+    for i in range(pass_num):
+        # When the customer has arrived at the restaurant and is pushed into the queue
+        Passenger.wait_time_queue = queue_len * serving_time
+        queue1.enqueue(Passenger.getPass(Passenger))
+        # When the customer at the counter has been served
+        if (not checkin_agent.isBusy() and (not queue1.isEmpty())):
+            nextPass = queue1.dequeue()
+            waiting_time = Passenger.wait_time_queue + agent_rate
+            wait_Times.append(waiting_time)
+            # print(waiting_Times)
+            # calculates the wait time for each customer and appends it to the list of waitingTimes
+        # print(queue)
+        checkin_agent.setIdle()  # current customer has now left and the cashier is available to serve the next customer
+
+
+
+    pass_num=flight_passenger3
+    no_of_pass = int(pass_num / agent_num)
+    queue_len = Passenger.queue_length(Passenger, no_of_pass)
+
+    for i in range(pass_num):
+        # When the customer has arrived at the restaurant and is pushed into the queue
+        Passenger.wait_time_queue = queue_len * serving_time
+        queue1.enqueue(Passenger.getPass(Passenger))
+        # When the customer at the counter has been served
+        if (not checkin_agent.isBusy() and (not queue1.isEmpty())):
+            nextPass = queue1.dequeue()
+            waiting_time = Passenger.wait_time_queue + agent_rate
+            wait_Times.append(waiting_time)
+            # print(waiting_Times)
+            # calculates the wait time for each customer and appends it to the list of waitingTimes
+        # print(queue)
+        checkin_agent.setIdle()  # current customer has now left and the cashier is available to serve the next customer
+
+
+
+
     avg_waiting_time=(sum(wait_Times)/len(wait_Times))/60
     return avg_waiting_time
 
@@ -225,7 +268,7 @@ if __name__ == '__main__':
 
         #print(wait_list)
         avg_time_case1 = (sum(wait_list1) / len(wait_list1))
-        print('Case 1:Average waiting time in the queue for a passenger is',avg_time_case1)
+       # print('Case 1:Average waiting time in the queue for a passenger is',avg_time_case1)
 
 
 
@@ -242,53 +285,32 @@ if __name__ == '__main__':
 
         #print(df)
         df1 = df.groupby(['No_of_passengers', 'No_of_agents'])[['Average_waiting_time']].mean()
-        print(df1)
+        #print(df1)
+
+
+        for c3 in range(10000):
+            flight=Flight.gen_flight(Flight)
+            flight_passenger1 = flight[0]
+            flight_passenger2=flight[1]
+            flight_passenger3=flight[2]
+            agent_num=Agent_Serves_Passenger.getAgent(Agent_Serves_Passenger)
+            agent_rate=Agent.get_Agent_Rate(Agent)
+
+            case3=flight_simulator(flight_passenger1,flight_passenger2,flight_passenger3,agent_rate,agent_num)
+
+            wait_list3 = [[flight_passenger1,flight_passenger2,flight_passenger3,agent_num,case3]]
+            df = df.append(
+                pd.DataFrame(wait_list3, columns=['Flight1', 'Flight2', 'Flight3','No_of_agents','Average_waiting_time']),
+                ignore_index=True,sort=True)
+
+        # df['Average_waiting_time']  = df['Average_waiting_time'] / 60
+
+        # print(df)
+        df2 = df.groupby(['Flight1', 'Flight2', 'Flight3','No_of_agents'])[['Average_waiting_time']].mean()
+        print(df2)
 
 
 
 
-
-
-
-
-
-
-
-
-        # for c3 in range(1000):
-        #     #pass
-        #     flight_sim=Flight.get_Flight(Flight)
-        #     agent_sim=Agent_Serves_Passenger.getAgent(Agent_Serves_Passenger)
-        #     case3=simulate(flight_sim,agent_sim)
-        #
-        #
-        #
-        #
-        # wait_list3=[[flight_sim,agent_sim,case3]]
-        # df = df.append(pd.DataFrame(wait_list2,columns=['No_of_passengers', 'No_of_agents','No_of_flights','Average_waiting_time']),ignore_index=True)
-        #
-        # df1 = df.groupby(['No_of_passengers', 'No_of_agents'])[['Average_waiting_time']].sum()
-        # print(df1)
-
-        # call_passenger=Passenger()
-        # no_of_passenger = call_passenger.no_of_passenger()
-        #
-        # print('Number of passengers are:',no_of_passenger)
-        #
-        # call_agent = Agent()
-        # no_of_agent=call_agent.no_of_agent()
-
-        # print('Number of agents are:',no_of_agent)
-
-        # call_flight = Flight()
-        # no_of_flight=call_flight.no_of_flight()
-        #
-        # print('Number of flights and their inter-arrival time are:',no_of_flight)
-        #
-
-
-# 215 passengers 3 check in counters
-# queue entry exit how much time in queue
-# check in : same rates 10 pass per hour 15 20 ?
 
 
